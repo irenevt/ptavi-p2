5c8c0a6925571b5e3d2243929f1d8c06e322cbc4
irenevt <i.vinagre.2016@alumnos.urjc.es>
2016-09-26 18:20:14 +0200

    Ejercicio 6 P1

--- calcplus.py
+++ calcplus.py
@@ -6,35 +6,35 @@ import calcoohija
 import calcoo
 
 
-mifichero = open('fichero.csv')
+ficheroOperaciones = open('fichero.csv')
 
-lineas = mifichero.readlines()
+lineas = ficheroOperaciones.readlines()
 
 if __name__ == "__main__":
 
     calcplus = calcoohija.CalculadoraHija()
     for linea in lineas:
-        #lista = linea[:-1].split(',')
 
-        operaciones = linea.split(',')[0]
-        numbers = linea.split(',')[1:]
-        result = int(numbers[0])
+        numeros = linea.split(',')[1:]        
+        operacion = linea.split(',')[0]
+        result = int(numeros[0])
 
-        if operation == "suma":
-            for i in range(1, len(numbers)):
-                result = calcplus.sum(result, int(numbers[i]))
-        elif operation == "resta":
-            for i in range(1, len(numbers)):
-                result = calcplus.rest(result, int(numbers[i]))
-        elif operation == "multiplica":
-            for i in range(1, len(numbers)):
-                result = calcplus.mult(result, int(numbers[i]))
-        elif operation == "divide":
-            for i in range(1, len(numbers)):
-                if int(numbers[i]) == "0":
-                    sys.exit("Division by zero is not allowed.")
+        if operacion == "suma":
+            for i in range(1, len(numeros)):
+                result = calcplus.sumar(result, int(numeros[i]))
+        elif operacion == "resta":
+            for i in range(1, len(numeros)):
+                result = calcplus.restar(result, int(numeros[i]))
+        elif operacion == "multiplica":
+            for i in range(1, len(numeros)):
+                result = calcplus.multiplicar(result, int(numeros[i]))
+        elif operacion == "divide":
+            for i in range(1, len(numeros)):
+                if int(numeros[i]) == "0":
+                    sys.exit("No se puede dividir entre cero")
                 else:
-                    result = calcplus.div(result, int(numbers[i]))
+                    result = calcplus.dividir(result, int(numeros[i]))
         else:
-            sys.exit("Gramatical Error.Try again.")
+            sys.exit("Escribe de nuevo: Suma, resta, multiplica o divide")
+
         print(result)
